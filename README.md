04b90382bbdc694c14376a6ef06fb76ae7709dd4
SagarTrimukhe <sagarltrimukhe@gmail.com>
2023-05-10 22:27:04 +0530

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # simple-iot-project
+A simple project that demonstrates on how to setup a simple IoT application using AWS IoT core, AWS Greengrass.
+
+Below is the high level architecture.
+
+
+
+Requirements:
+1. A Virtual machine. (Host computer can also be used but VM is preferred)
+2. AWS CLI
+3. Python runtime
+4. Laptop with webcam
+5. Install the Oracle extension pack to attach the host webcam to VM:
+
+
+Steps to setup and run the project
+1. Create a s3 bucket.
+Copy the artificats (Python code) to the s3 bucket
+
+aws s3 cp  AVPubSub.py s3://autonomous-vehicle-iot-proj-bucket/artifacts/AVPubSub.py
+
+2. Create a AWS user.
+    av-edge-server-management-user
+
+    Create Access key, secret key for the user and export them as env vars in the VM.
+
+3. Create and attach policy to the user.
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "logs:DescribeLogStreams",
+                "s3:GetBucketLocation",
+                "greengrass:*",
+                "iot:*",
+                "s3:*",
+                "iam:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+```
+
+
+4. Add a new core device in AWS IoT Core.
+    Follow the instrcution 
+    Core-device-name: AV_Edge_Server
+    Create a new group or reuse existing group.
+
+A new IAM policy will be created once the greengrass is installed on the VM
+Make sure to update that policy by giving access to s3 bucket creted. Read access
+
+
+5. Create new component:
+    The receipe can be found at this location.
+
+5. Create new Deployment:
+    name: AV_Edge_Server_Deployment
+    Mention the Core device added in previous step.
+    Select components:
+        AV_PubSub
+        aws.greengrass.nucleus
+        aws.greengrass.Cli
+
+    The code from the s3 bucket will be deployed to the edge server
+
+    Run below command in the VM to check logs 
+    sudo tail -F  /greengrass/v2/logs/AV_PubSub.log
+
---
+++ artifacts/AVPubSub.py
@@ -0,0 +1,119 @@
+
+from datetime import datetime
+import time
+import traceback
+import json
+import boto3
+import botocore
+import sys
+import os
+
+import awsiot.greengrasscoreipc
+import awsiot.greengrasscoreipc.client as client
+from awsiot.greengrasscoreipc.model import (
+    IoTCoreMessage,
+    QOS,
+    SubscribeToIoTCoreRequest,
+    PublishToIoTCoreRequest
+)
+
+from cv2 import VideoCapture, imshow, imwrite, waitKey, destroyWindow
+
+TIMEOUT = 10
+BUCKET = sys.argv[1]
+REQUEST_TOPIC = sys.argv[2]
+RESPONSE_TOPIC = sys.argv[3]
+THING_NAME = os.getenv('AWS_IOT_THING_NAME')
+
+ipc_client = awsiot.greengrasscoreipc.connect()
+
+
+def respond(event):
+    # Capture image
+    take_picture()
+    image_url = upload_file(BUCKET, event.message.payload)
+    response_message = {
+        "timestamp": int(round(time.time() * 1000)),
+        "image": image_url
+    }
+
+    # Publish to our topic
+    response = PublishToIoTCoreRequest()
+    response.topic_name = RESPONSE_TOPIC
+    response.payload = bytes(json.dumps(response_message), "utf-8")
+    response.qos = QOS.AT_MOST_ONCE
+    response_op = ipc_client.new_publish_to_iot_core()
+    response_op.activate(response)
+
+
+def take_picture():
+    cam_port = 0
+    cam = VideoCapture(cam_port)
+    
+    # reading the input using the camera
+    result, image = cam.read()
+    
+    # If image will detected without any error, 
+    # show result
+    if result:  
+        # saving image in local storage
+        imwrite("photo.png", image)  
+    # If captured image is corrupted, moving to else part
+    else:
+        print("No image detected. Please! try again")
+
+def upload_file(bucket, pic_filepath):
+    # Upload the file to S3.  Note that I'm setting a PUBLIC-READ ACL just for ease of troubleshooting.
+    # You probably would not want to do this unless it's truly a public endeavour.
+    try:
+        s3_client = boto3.client('s3')
+        pic_filepath = "photo.png"
+
+        filename = RESPONSE_TOPIC + '/' + THING_NAME + '/' + \
+            str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.jpg'
+        s3_client.upload_file(pic_filepath, bucket, filename,
+                              ExtraArgs={'ACL': 'public-read'})
+        url = 'https://{}.s3.amazonaws.com/{}'.format(BUCKET, filename)
+
+    except botocore.exceptions.ClientError as e:
+        url = e.response
+
+    finally:
+        return url
+
+
+class StreamHandler(client.SubscribeToIoTCoreStreamHandler):
+    def __init__(self):
+        super().__init__()
+
+    def on_stream_event(self, event: IoTCoreMessage) -> None:
+        try:
+            respond(event)
+        except:
+            traceback.print_exc()
+
+    def on_stream_error(self, error: Exception) -> bool:
+        # Handle error.
+        return True  # Return True to close stream, False to keep stream open.
+
+    def on_stream_closed(self) -> None:
+        # Handle close.
+        pass
+
+# Setup the MQTT Subscription
+
+
+request = SubscribeToIoTCoreRequest()
+request.topic_name = REQUEST_TOPIC
+request.qos = QOS.AT_MOST_ONCE
+handler = StreamHandler()
+operation = ipc_client.new_subscribe_to_iot_core(handler)
+future = operation.activate(request)
+future.result(TIMEOUT)
+
+# Keep the main thread alive, or the process will exit.
+while True:
+    time.sleep(10)
+
+# To stop subscribing, close the operation stream.
+operation.close()
